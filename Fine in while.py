5e3a108b283ef61b3e10bc494c3c7ac916f2d713
Charan Naidu <ncharannaidu087@gmail.com>
2025-07-04 14:30:57 +0530

    Add Fine in while.py file

---
+++ Fine in while.py
@@ -0,0 +1,6 @@
+Fine =int(input("Enter Your Fine Amount: "))
+while Fine<0:
+    print("Fine cant be Negative")
+    age =int(input("Enter Your Fine: "))
+     
+print(f"Your Fine is {Fine}₹ Rupees")     
