1afe8cba7ebcbd1641dc503a3dc2f615e4d21a19
Charan Naidu <ncharannaidu087@gmail.com>
2025-07-07 13:57:30 +0530

    Add 2D collections files

---
+++ 2D List.py
@@ -0,0 +1,7 @@
+fruits =     ["apple", "orange", "bananna", "coconut"]
+vegitables = ["celery", "carrots", "potatos"]
+meats =      ["chicken", "fish", "turkey"]
+
+groceries = [fruits, vegitables, meats]
+
+print(groceries[1][2])
---
+++ Tupple Sets.py
@@ -0,0 +1,9 @@
+groceries = [{"apple", "orange", "bananna", "coconut"},
+             ("celery", "carrots", "potatos"),
+             ["chicken", "fish", "turkey"]]
+
+for collection in groceries:
+    #print(collection)
+     for food in collection:
+          print(food, end=" ")
+     print()     
